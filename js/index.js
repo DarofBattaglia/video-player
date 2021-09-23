e8bdd8a6f074c34ae2e235042a02d0ac082a68da
DarofBattaglia <91293250+DarofBattaglia@users.noreply.github.com>
2021-09-23 20:02:24 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,61 @@
+const $video = document.querySelector("#video")
+    const $play = document.querySelector("#play")
+    const $pause = document.querySelector("#pause")
+    const $backward = document.querySelector("#backward")
+    const $forward = document.querySelector("#forward")
+    
+    $play.addEventListener("click", handlePlay)
+
+    function handlePlay() {
+    $video.play()
+    $play.hidden = true
+    $pause.hidden = false
+    console.log("le diste click")
+    }
+
+    $pause.addEventListener("click", handlePause)
+
+    function handlePause() {
+    $video.pause()
+    $pause.hidden = true
+    $play.hidden = false
+    console.log("le diste click")
+    }
+
+
+    $backward.addEventListener("click", handlebackward)
+
+    function handlebackward() {
+        $video.currentTime -= 10
+
+        console.log("atras 10 seg", $video.currentTime)
+
+    }
+
+    $forward.addEventListener("click", handleforward)
+   
+    function handleforward() {
+        $video.currentTime += 10
+        console.log("adelante 10 seg", $video.currentTime)
+    }
+
+    const $progress = document.querySelector("#progress")
+    $video.addEventListener("loadedmetadata", handleloaded)
+    $video.addEventListener("timeupdate", handleTimeUpdate)
+
+    function handleloaded() {
+        $progress.max = $video.duration
+        console.log("ha cargado mi video", $video.duration)
+    }
+
+    function handleTimeUpdate() {
+        $progress.value = $video.currentTime
+        // console.log("holi", $video.currentTime)
+    }
+
+    $progress.addEventListener("input", handleInpud)
+
+    function handleInpud() {
+        $video.currentTime = $progress.value
+        console.log($progress.value)
+    }
